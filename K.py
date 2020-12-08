918147af4b14a8de76d8c04e02a675a5bd4af577
r-baaz <58431272+r-baaz@users.noreply.github.com>
2020-12-08 16:18:18 +0530

    Update K.py

--- K.py
+++ K.py
@@ -1,10 +1,12 @@
-dic1={1:10, 2:20}
-dic2={3:30, 4:40}
-dic3={5:50,6:60}
-print("dic1 :",dic1)
-print("dic2 :",dic2)
-print("dic3 :",dic3)
-dic1.update(dic2)
-dic1.update(dic3)
-print("after concatenate dic1,dic2,dic3 :")
-print(dic1)
+class student:
+     def __init__(self,name,rollno,dep,city):
+            self.name=name
+            self.rollno=rollno
+            self.dep=dep
+            self.city=city
+     def display(self): 
+         print("name:",self.name,"rollno:",self.rollno,"dep",self.dep,"city",self.city)
+
+t=student("Arbaaz",8,"IT","Mumbai")
+st.display()
+
