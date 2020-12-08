6e5c44a3a43886c0cb18da1e017950c50911b1db
r-baaz <58431272+r-baaz@users.noreply.github.com>
2020-12-08 16:29:19 +0530

    Update K.py

--- K.py
+++ K.py
@@ -1,12 +1,9 @@
-class student:
-     def __init__(self,name,rollno,dep,city):
-            self.name=name
-            self.rollno=rollno
-            self.dep=dep
-            self.city=city
-     def display(self): 
-         print("name:",self.name,"rollno:",self.rollno,"dep",self.dep,"city",self.city)
-
-t=student("Arbaaz",8,"IT","Mumbai")
+class base:
+     def display(self):
+            print("base class")
+class child(base):
+     def show(self):
+            print("child class")
+st=child()
 st.display()
-
+st.show()
