0d10934945ff8a8607c872d64febf5ca07627dcf
r-baaz <58431272+r-baaz@users.noreply.github.com>
2020-12-08 15:05:46 +0530

    Update K.py

--- K.py
+++ K.py
@@ -1,14 +1,10 @@
-def strlenF(c):
-       count=0
-       for i in c:
-             count=count+1
-       print("the lenth is",count)
-c=str(input("enter a string"))
-strlenF(c)
-n=int(input("Enter the size of list"))
-lst=[]
-for i in range(0,n):
-	k=input("enter list element")
-	lst.append(k)
-	
-strlenF(lst)
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+print("dic1 :",dic1)
+print("dic2 :",dic2)
+print("dic3 :",dic3)
+dic1.update(dic2)
+dic1.update(dic3)
+print("after concatenate dic1,dic2,dic3 :")
+print(dic1)
