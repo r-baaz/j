d0e894f3a0657897949b8b0c0f64a0cce7a2fd5b
r-baaz <58431272+r-baaz@users.noreply.github.com>
2020-12-10 01:41:19 +0530

    Delete K.py

--- K.py
+++
@@ -1,9 +0,0 @@
-class base:
-     def display(self):
-            print("base class")
-class child(base):
-     def show(self):
-            print("child class")
-st=child()
-st.display()
-st.show()
